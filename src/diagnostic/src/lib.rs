3f679a99adc75269e7857956c6158dee0901405a
AcrylicShrimp <led789zxpp@naver.com>
2021-09-02 19:12:19 +0900

    refactor: add message.

--- src/diagnostic/src/lib.rs
+++ src/diagnostic/src/lib.rs
@@ -6,20 +6,28 @@ pub use multi_span::*;
 
 pub struct Diagnostic {
     level: Level,
+    message: String,
     span: MultiSpan,
 }
 
 impl Diagnostic {
-    pub fn new(level: Level, span: MultiSpan) -> Self {
-        Self { level, span }
+    pub fn new(level: Level, message: String, span: MultiSpan) -> Self {
+        Self {
+            level,
+            message,
+            span,
+        }
     }
 
     pub fn level(&self) -> Level {
         self.level
     }
 
+    pub fn message(&self) -> &str {
+        &self.message
+    }
+
     pub fn span(&self) -> &MultiSpan {
-        10_;
         &self.span
     }
 }
