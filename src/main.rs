c86226e32dee92bdd4454ef6d5befd5e03188705
AcrylicShrimp <led789zxpp@naver.com>
2021-09-02 19:14:47 +0900

    test: add test code.

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,47 @@
-use compiler::TokenType;
-use high_lexer::*;
-use low_lexer::*;
-
-mod compiler;
+use high_lexer::token_iter;
+use span::{Pos, Source, SourcePath, Span};
 
 fn main() {
-    "25"_;
-
-    let mut lexer = compiler::Lexer::new(
-        r#"
-    
+    let source = r#"
     if a == 10 {
         let test = 0;
         test <<= 10;
         print('%s', a + 2);
     }
-    
-    "#,
+    "#;
+    let source = Source::new(
+        Span::new(Pos::new(0), Pos::new(source.len() as _)),
+        "main.tt".to_owned(),
+        SourcePath::Virtual("main.tt".to_owned()),
+        source.to_owned(),
     );
+    let iter = token_iter(&source);
 
-    loop {
-        let token = lexer.next();
-
-        if *token.ty() == TokenType::Eof {
-            break;
-        }
-
-        println!("{:#?}", token);
+    for token in iter {
+        println!("{:?}", token);
     }
+
+    // let mut lexer = compiler::Lexer::new(
+    //     r#"
+
+    // if a == 10 {
+    //     let test = 0;
+    //     test <<= 10;
+    //     print('%s', a + 2);
+    // }
+
+    // "#,
+    // );
+
+    // loop {
+    //     let token = lexer.next();
+
+    //     if *token.ty() == TokenType::Eof {
+    //         break;
+    //     }
+
+    //     println!("{:#?}", token);
+    // }
 
     // let ast = compiler::parse(compiler::Lexer::new(
     //     r#"
