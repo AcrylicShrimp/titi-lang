ee9d4f7efa49f03c8e14fdb7eac89749ecb7b981
AcrylicShrimp <led789zxpp@naver.com>
2021-09-02 19:10:13 +0900

    feat: add quotes when write for debug.

--- src/str_interner/src/str_idx.rs
+++ src/str_interner/src/str_idx.rs
@@ -40,7 +40,7 @@ impl From<StrIdx> for u32 {
 #[cfg(feature = "global_instance")]
 impl Debug for StrIdx {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.as_str())
+        write!(f, "\"{}\"", self.as_str())
     }
 }
 
